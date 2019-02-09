4bd99ef1a1c46d87e421af9c8f74f60f6452d1d0
Markus Szokoll <markus.szokoll@gmx.at>
2019-02-09 16:55:40 +0100

    working sort

--- js/index.js
+++ js/index.js
@@ -1,7 +1,12 @@
 $(document).ready(function() {
 	var movies = JSON.parse(jsonMovies);
+	var sort1 = false;
+	var sort2 = false;
+	var sortType = false;
 	
-	document.getElementById("sort").addEventListener("click",sort,false)
+	document.getElementById("sort1").addEventListener("click", function(){sort("name");},false);
+	document.getElementById("sort2").addEventListener("click", function(){sort("likes");},false);
+	document.getElementById("sortType").addEventListener("click", function(){sort("type");},false);
 
 
 	function createContent(what){
@@ -21,8 +26,16 @@ $(document).ready(function() {
 			});
 		}
 		else if(what=="name"){
-			movieArray.sort();
+			movieArray.sort(function(a,b) {
+				if(a[1].charCodeAt(0)==b[1].charCodeAt(0)){
+					return a[1].charCodeAt(1)-b[1].charCodeAt(1)
+				}
+				else{
+   					return a[1].charCodeAt(0)-b[1].charCodeAt(0)
+   				}
+			});
 	    }
+	    if(sortType){movieArray.reverse()}
 
 
 
@@ -37,7 +50,7 @@ $(document).ready(function() {
 							<img height="350px" src="img/${movies[j].image}"></img>
 						</div>
 						<div class="col-lg-6 col-md-6 col-sm-6 col-xs-6 shortInfo">
-							${movies[j].name}<br><br>${movies[j].overview}<br>${movies[j].likes}
+							<h3>${movies[j].name}</h3><br><br>${movies[j].overview}<br><button id="id${j}">LIKE</button><span id="span${j}">${movies[j].likes}</span>
 						</div>
 					</div>
 				</div>
@@ -45,11 +58,73 @@ $(document).ready(function() {
 		}
 		content += `</div>`;
 		document.getElementById("content").innerHTML = content;
+
+		for(i=0;i<movies.length;i++){
+			var yyy = "id"+i.toString();
+			document.getElementById(yyy).addEventListener("click", function(){lb(i);},false);
+		
+		}
+
+		
 	}
 
+	function lb(real){
+		movies[real].likes += 1;
+		sort("justdo");
+		var yyy = "span"+real.toString();
+		document.getElementById(yyy).innerHTML += 1;
+	}
 	
 
-	createContent("name");
+	function sort(what){
+		if(what=="type"){
+			sortType = !sortType;
+			if(!sortType){
+				document.getElementById("sortType").style.background = "white";
+				document.getElementById("sortType").innerHTML = "ASCENDING";
+			}
+			else{
+				document.getElementById("sortType").style.background = "yellow";
+				document.getElementById("sortType").innerHTML = "DESCENDING";
+			}
+			if(sort1){
+				createContent("name");
+			}
+			else if(sort2){
+				createContent("likes");
+			}
+		}
+		else if(what=="justdo"){
+			if(sort1){
+				createContent("name");
+			}
+			else if(sort2){
+				createContent("likes");
+			}
+		}
+		else if(what=="name"){
+			if(!sort1){
+				sort1=true;
+				sort2=false;
+				document.getElementById("sort1").style.background = "yellow";
+				document.getElementById("sort2").style.background = "white";
+				createContent("name");
+			}
+		}
+		else if(what=="likes"){
+			if(!sort2){
+				sort1=false;
+				sort2=true;
+				document.getElementById("sort1").style.background = "white";
+				document.getElementById("sort2").style.background = "yellow";
+				createContent("likes");
+			}
+		}
+		}
+	
+	sort("name");
+
+
 
 })
 
